3a302039aee8f1fb7714925814efbe721b37aeb8
Michel Bolzon <michel.bolzon123@Gmail.com>
2019-04-12 18:44:12 -0300

    #

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '35.227.58.110'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'site_dinamico'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', '1525605'),
+            'password' => env('DB_PASSWORD', '4038798'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
